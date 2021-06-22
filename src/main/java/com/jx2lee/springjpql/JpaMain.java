5fb212e3f3dc3dffb10efd13864ecacb81344c0a
jx2lee <jaejun.lee.1991@gmail.com>
2021-06-22 19:49:00 +0900

    섹션 11. 객체지향 쿼리언어2 - 중급 문법: fetch join 기본
    * 완전 중요
    * fetch join?
      * join 종류 아님
      * JPQL 성능 최적화를 위한 기능
      * 연관된 엔티티 & 컬렉션을 하나의 SQL 로 조회
      * ex) select m from Member m join fetch m.team
    
    Hibernate:
        /* select
            m
        from
            Member m
        join
            fetch m.team */ select
                member0_.id as id1_0_0_,
                team1_.id as id1_3_1_,
                member0_.age as age2_0_0_,
                member0_.TEAM_ID as team_id5_0_0_,
                member0_.type as type3_0_0_,
                member0_.username as username4_0_0_,
                team1_.name as name2_3_1_
            from
                Member member0_
            inner join
                Team team1_
                    on member0_.TEAM_ID=team1_.id
    
    * 컬렉션 페치 조인 시 (일대다): 일 엔티티에서 다 엔티티로 조인한 경우 데이터 중복 현상이 발생할 수 있음
      * DISTINCT 제공
      * SQL 에 distinct 생성 -> 실제 애플리케이션 동작 시 같은 엔티티가 존재하는 경우 제거 (SQL 문에 distinct 포함되었다고 중복이 제거되지 않음. 같은 식별자를 가진 다 엔티티를 제거해야지만 중복 제거가 가능함)
    * fetch join vs join(general)
      * 일반 조인은 연관된 엔티티를 함께 조회하지 않음 (select 필드에 조인한 엔티티 필드를 가져오지 않음)
      * fetch join 은 연관된 엔티티를 함께 조회 (지연로딩이어도 즉시 로딩으로 바로 가져옴)
      * fetch join: 객체 그래프를 하나의 SQL 로 탐색하는 개념

--- src/main/java/com/jx2lee/springjpql/JpaMain.java
+++ src/main/java/com/jx2lee/springjpql/JpaMain.java
@@ -15,32 +15,60 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Team team = new Team();
-            team.setName("test");
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자A");
-            member.setAge(20);
-            member.setTeam(team);
-            member.setType(MemberType.ADMIN);
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            member1.setType(MemberType.ADMIN);
+            em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자B");
-            member2.setAge(30);
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             member2.setType(MemberType.ADMIN);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            member3.setType(MemberType.ADMIN);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            // 명시적 조인 예시
-            String sizeQuery = "select m.username from Team t join t.members m";
+            // fetch join
+            String fetchQuery = "select m from Member m join fetch m.team";
+            List<Member> result = em.createQuery(fetchQuery, Member.class)
+                    .getResultList();
 
-            Collection result = em.createQuery(sizeQuery, String.class).getResultList();
-            System.out.println("result = " + result);
+            for (Member member : result) {
+                System.out.println("member.getUsername() = " + member.getUsername() + ", " + member.getTeam().getName());
+                // 회원1 -> 팀A(SQL)
+                // 회원2 -> 팀A(1차캐시)
+                // 회원3 -> 팀B(SQL)
+                // -> N + 1 문제: fetch 로 해결 가능
+            }
+
+            // fetch join - 컬렉션 패치조인 (일대다 관계)
+            // 뻥튀기 조심 -> distinct 로 우회가능
+            String fetchOneToManyQuery = "select distinct t from Team t join fetch t.members";
+            List<Team> teams = em.createQuery(fetchOneToManyQuery, Team.class)
+                    .getResultList();
+
+            for (Team team : teams) {
+                System.out.println("team.getName() = " + team.getName() + ", " + team);
+                for (Member member : team.getMembers()) {
+                    System.out.println("==> member = " + member.getUsername() + ", " + member);
+                }
+            }
 
             tx.commit();
 
